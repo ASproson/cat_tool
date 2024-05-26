b12112e4df2f401846d4e62df692590e297ba4c7
ASproson <a.sproson@outlook.com>
2024-05-26 10:55:18 +0100

    Imports abstracted funcs

--- main.js
+++ main.js
@@ -1,55 +1,4 @@
-const fs = require("fs");
-const readline = require("readline");
-
-/**
- * Reads contents of file from the passed file path and prints them out
- * @param {*} filePath
- */
-function readFromFile(filePath) {
-  readline.createInterface({
-    input: fs.createReadStream(filePath),
-    output: process.stdout,
-  });
-}
-
-/**
- * Accepts an array of file paths and concatenates the result
- * @param {*} filePaths
- */
-function concatenateFiles(filePaths) {
-  filePaths.forEach((path) => {
-    fs.readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        console.error("Error reading file from path: ", err);
-        return;
-      }
-      console.log(data);
-    });
-  });
-}
-
-/**
- * Output file at filepath with numbered lines
- * @param {*} filePath
- */
-function numberLines(filePath, includeBlankLines) {
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      console.log("Error reading file: ", err);
-      return;
-    }
-    const lines = data.split("\n");
-    let lineNumber = 1;
-    lines.forEach((line) => {
-      if (line.trim() !== "" || includeBlankLines) {
-        console.log(`${lineNumber} ${line}`);
-        lineNumber++;
-      } else {
-        console.log();
-      }
-    });
-  });
-}
+const { readFromFile, concatenateFiles, numberLines } = require("./utils.js");
 
 function main() {
   const args = process.argv.slice(2);
@@ -93,8 +42,6 @@ function main() {
 }
 
 main();
-
-module.exports = { readFromFile, concatenateFiles, numberLines };
 
 // node main.js readline test.txt
 // node main.js cat test.txt test2.txt
